e3a1c1298a1d8d81aade2fd32d0b58a2a817b2fb
fringel <blaise@njorku.com>
2015-11-19 01:34:38 +0100

    Fixed crash for pre Lollipop devices
    Fixed Categories now displaying

--- app/src/main/java/com/iceteck/hivote/CatergoriesActivity.java
+++ app/src/main/java/com/iceteck/hivote/CatergoriesActivity.java
@@ -1,15 +1,14 @@
 package com.iceteck.hivote;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.multidex.MultiDex;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -65,6 +64,8 @@ public class CatergoriesActivity extends AppCompatActivity
 
     private void initRecyclerView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(layoutManager);
         setupAdapter();
     }
 
